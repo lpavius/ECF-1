a2e795abcbacbcaab2c72cf3746f0726c76df6a3
lpavius <lpavius@student.42.fr>
2020-05-28 11:07:16 +0200

    email unique

--- backend/src/main/java/com/lucypavius/backend/entities/User.java
+++ backend/src/main/java/com/lucypavius/backend/entities/User.java
@@ -20,7 +20,7 @@ public class User {
     @Column(nullable = false)
     private LocalDate birthDate;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String email;
 
     @Column(nullable = false, length = 60)
